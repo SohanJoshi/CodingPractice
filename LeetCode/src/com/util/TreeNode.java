47c39d87d452cf181e9108bc675ed57870d9f506
SohanJoshi <sohanjoshi1991@gmail.com>
2024-01-31 13:26:01 +0530

    Back to basics

--- LeetCode/src/com/util/TreeNode.java
+++ LeetCode/src/com/util/TreeNode.java
@@ -1,8 +1,14 @@
 package com.util;
 
 public class TreeNode {
-	public int val;
-	public TreeNode left;
-	public TreeNode right;
-	public TreeNode(int x) { val = x; }
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode() {}
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
